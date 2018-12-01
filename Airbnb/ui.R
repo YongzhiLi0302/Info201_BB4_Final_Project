92e9fd3b9172a42849b593047b023ecc05fe5c7e
mirandasiyaoz <siyaoz3@uw.edu>
2018-11-30 20:00:39 -0800

    layout

--- Airbnb/ui.R
+++ Airbnb/ui.R
@@ -3,27 +3,35 @@ library(dplyr)
 library(shinyWidgets)
 library(shinythemes)
 
+library(shiny)
+library(dplyr)
+library(shinyWidgets)
+library(shinythemes)
+
 shinyUI(fluidPage( 
   navbarPage("Aribnb in Seattle",
              theme = shinythemes::shinytheme("slate"),  # <--- Specify theme here
-             tabPanel(title = "Home", textOutput("welcome")),
-             tabPanel(title = "Plot", sidebarPanel(
+             tabPanel(title = "Home", textOutput("welcome message and getting started")),
+             tabPanel(title = "Seattle's Map", sidebarPanel(
                textInput("txt", "Text input:", "text here"),
                sliderInput("slider", "Slider input:", 1, 100, 30),
                actionButton("action", "Button"),
                actionButton("action2", "Button2", class = "btn-primary")
              )),
+             tabPanel(title = "Seattle's listing", textOutput("welcome")),
+             tabPanel(title = "Seattle's neighbourhoods", textOutput("welcome")),
+             tabPanel(title = "Seattle's reviews", textOutput("welcome")),
              navbarMenu("More",
-                        tabPanel("Summary", textOutput("Summary")),
-                        tabPanel("Table", "Table tab contents...")
+                        tabPanel("Contact Us", textOutput("collaborators info")),
+                        tabPanel("Q&A", "some common issues")
              )
-    )
   )
+)
 )
 
 panel1 <- 
-mainPanel(
-  tabsetPanel(
-    tabPanel("Plot", plotOutput("plot")),
-    tabPanel("Map", plotOutput("map")),
-    tabPanel("Table", plotOutput("table"))))
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot", plotOutput("plot")),
+      tabPanel("Map", plotOutput("map")),
+      tabPanel("Table", plotOutput("table"))))
