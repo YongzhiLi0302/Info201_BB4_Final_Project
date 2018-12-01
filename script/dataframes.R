548860567174649043869e6d69eda314a4fe5656
Lint <lint272@uw.edu>
2018-12-01 15:15:55 -0800

    add dataframes we will work on

--- script/dataframes.R
+++ script/dataframes.R
@@ -4,6 +4,5 @@ calendar_df <- read.csv("../data/calendar.csv.gz", stringsAsFactors = FALSE)
 
 listing_df <- read.csv("../data/listings.csv.gz", stringsAsFactors = FALSE)
 
-listing_other <- read.csv("../data/listings.csv", stringsAsFactors = FALSE)
-
 review_df <- read.csv("../data/reviews.csv.gz", stringsAsFactors = FALSE)
+
