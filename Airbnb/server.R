b2976628569e4b5510d211ab0ea37fdb6a92a1fb
LiYongzhi <LiYongzhi@LiYongzhideMacBook-Air.local>
2018-12-01 23:10:27 -0800

    add interactive

--- Airbnb/server.R
+++ Airbnb/server.R
@@ -21,17 +21,28 @@ shinyServer(function(input,output) {
   
   output$neighbourhood <- renderUI ({
     neighbourhood_distinct <- distinct(listings2_df, neighbourhood_group_cleansed, keep_all = FALSE)
-    selectInput("neighbourhood_group", "Neighbourhood Group", as.list(select(neighbourhood_distinct, neighbourhood_group_cleansed)))
+    selectInput("neighbourhood_group", "Neighbourhood Group", c("All", as.list(select(neighbourhood_distinct, neighbourhood_group_cleansed))))
                 
   }) 
   
+  # Reactive expression for the data subsetted to what the user selected
+  filtered_Neighbourhood <- reactive({
+    if (input$neighbourhood_group == "All"){
+      listings2_df
+    } else {
+      filter(listings2_df, neighbourhood_group_cleansed == input$neighbourhood_group)
+    }
+  })
+  
+  
   # Create the map
   output$map <- renderLeaflet({
-    leaflet() %>%
+    df <- filtered_Neighbourhood()
+    leaflet(data = df) %>%
       addTiles() %>%  # Add default OpenStreetMap map tiles
-      setView(lng = -122.3321, lat = 47.6062, zoom = 12) %>% # Set default view at Seattle 
-      addCircles(lng = listings2_df$longitude, lat = listings2_df$latitude, radius = 10, color = "#18BC9C",
-                  weight = 3, fillOpacity = 0.9) 
+      setView(lng = -122.3321, lat = 47.6062, zoom = 12) %>%  # Set default view at Seattle 
+      addCircles(lng = ~longitude, lat = ~latitude, radius = 10, color = "#18BC9C",
+               weight = 3, fillOpacity = 0.9)
   })
   
 
