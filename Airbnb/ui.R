dbc874b8e32ddc6c7eabc048b87b474a8d2c10f4
mirandasiyaoz <siyaoz3@uw.edu>
2018-12-01 22:44:22 -0800

    box plot

--- Airbnb/ui.R
+++ Airbnb/ui.R
@@ -45,14 +45,25 @@ shinyUI(fluidPage(
                                     uiOutput("neighbourhood")
                                     )
                       )),
-                      
-             
-             tabPanel(title = "Seattle's listing"),
-             
-             tabPanel(title = "Seattle's neighbourhoods"),
-             
-             tabPanel(title = "Seattle's reviews"),
-             
+             tabPanel(title = "Analysis", sidebarLayout(
+               sidebarPanel( tags$h1("House Type"),
+                             tags$h2("Apartment    35%"),
+                             tags$h3("  House    33%"),
+                             tags$h3("  Others    30%"),
+                             tags$h4("Airbnb hosts can list many types of homes, most common ones are houses and apartments.
+                                      Depending on the needs of traveller, they could easily see if there are sufficient type of home which they need;
+                                      meanwhile, hosts/potential hosts are able to see the competivity of each type of home in their local market.")),
+               mainPanel(plotOutput("housetype"))),
+               sidebarLayout(
+                 sidebarPanel(tags$h1("Room Type"),
+                              tags$h2("Entire Home/Apt   74%"),
+                              tags$h3("  Private Room    23%"),
+                              tags$h3("  Shared Room   1%"),
+                              tags$h4("Airbnb hosts can list many different types of rooms, including entire home/apt, private room and shared room.
+                                      Depending on the needs of traveller, they could easily see if there are sufficient type of room which they need;
+                                      meanwhile, hosts/potential hosts are able to see the competivity of each type of room in their local market.")),
+                 mainPanel(plotOutput("roomtype")
+                 ))),         
              navbarMenu("More",
                         tabPanel("Contact Us"),
                         tabPanel("Q&A", "common questions")
